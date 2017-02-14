19271a34cefa04f49f863ed145c221220855ee25
locke.wei <heart.like.frozenwater@gmail.com>
2017-02-14 18:24:24 +0800

    remove log in webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -46,8 +46,6 @@ module.exports = function (webpackConfig, env) {
     }
   })
 
-  console.dir(webpackConfig.module.loaders);
-
   // add resolve root fold
   webpackConfig.resolve.root = [
     path.resolve('./client'),
