04a6314522f32ab5eceb48a092d32f552329adab
locke.wei <heart.like.frozenwater@gmail.com>
2017-02-23 18:55:14 +0800

    add user model create method

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
-import db from '../lib/db';
+import db from '../lib/db'
 import util from '../lib/util'
-import log4js from 'log4js';
+import log4js from 'log4js'
+import _ from 'lodash'
 
 const LOG = log4js.getLogger('file');
 
@@ -12,6 +13,28 @@ export default {
   findById: async (id) => {
     const results = await db.query('select * from tv_users where id = ?', [id]);
     return results.length > 0 ? results[0] : null;
+  },
+
+  create: async (loginname, password, nicename, gender, phone, email) => {
+    const fields = [];
+    const now = new Date();
+    const values = [loginname, password, phone, now];
+    if (!_.isEmpty(nicename)) {
+      fields.push('user_nicename');
+      values.push(nicename);
+    }
+    if (!_.isEmpty(gender)) {
+      fields.push('user_gender');
+      values.push(nicename);
+    }
+    if (!_.isEmpty(email)) {
+      fields.push('user_email');
+      values.push(email);
+    }
+    const updateFields = fields.length ? `${fields.join(' , ')}, ` : ''
+    await db.query(`
+      insert into tv_users(user_login, user_pass, phone, user_registered, ${updateFields})
+    `, values);
   },
 
   verify: async (username, password) => {
